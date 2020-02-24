da7806b5df20a6c7a8c4ecfdbd1bb5e9f6088d2c
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-02-24 21:37:30 +0545

    ass

--- app/recipe_secondary/admin.py
+++ app/recipe_secondary/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'username']
+    list_filter = ['email', 'username']
+    search_fields = ['email', 'username']
+
+admin.site.register(UserAdmin, models.Account)
+# admin.site.register(models.Account)
